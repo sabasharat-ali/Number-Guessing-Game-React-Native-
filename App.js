47ede3fcdb7a00c1a4d20409d894c0fa94f8ec2d
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-06-19 22:56:29 +0500

    Added customized fonts

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
 import Header from "./components/header/header";
 import StartGameScreen from "./screens/StartGameScreen/startgamescreen";
 import GameScreen from "./screens/GameScreen/gamescreen";
 import GameOverScreen from "./screens/GameOverScreen/gameoverscreen";
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRound, setGuessRound] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setDataLoaded(true);
+        }}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   const startScreenHandler = (enteredNumber) => {
     setUserNumber(enteredNumber);
