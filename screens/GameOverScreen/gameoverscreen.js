dda5a2c40f8b837c00e180662cb9c2bc1ef75067
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-06-23 00:31:22 +0500

    added images

--- screens/GameOverScreen/gameoverscreen.js
+++ screens/GameOverScreen/gameoverscreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 import DefaultStyles from "../../constants/default-styles";
 
@@ -7,6 +7,20 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>THE GAME IS OVER</Text>
+      <View style={styles.imageContainer}>
+        {/* <Image
+          source={require("../../assets/Images/success.png")}
+          style={styles.image}
+        /> */}
+        <Image
+          source={{
+            uri:
+              "https://overseasadventuroustravel01.files.wordpress.com/2015/07/mountain-peaks.jpg?w=322&h=217",
+          }}
+          style={styles.image}
+          fadeDuration={300}
+        />
+      </View>
       <Text style={DefaultStyles.bodyText}>
         Your number was: {props.userNumber}
       </Text>
@@ -23,6 +37,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderColor: "black",
+    borderRadius: 150,
+    borderWidth: 3,
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
