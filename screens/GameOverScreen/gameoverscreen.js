253209cda50479b2f702697d461c9a3a534cb9ea
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-01 00:46:42 +0500

    added some generalized styling

--- screens/GameOverScreen/gameoverscreen.js
+++ screens/GameOverScreen/gameoverscreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 
 import DefaultStyles from "../../constants/default-styles";
 import Colors from "../../constants/colors";
@@ -7,14 +15,15 @@ import MainButton from "../../components/main-button/main-button";
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text style={DefaultStyles.title}>THE GAME IS OVER</Text>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../../assets/Images/success.png")}
-          style={styles.image}
-        />
-        {/* <Image
+    <ScrollView>
+      <View style={styles.screen}>
+        <Text style={DefaultStyles.title}>THE GAME IS OVER</Text>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../../assets/Images/success.png")}
+            style={styles.image}
+          />
+          {/* <Image
           source={{
             uri:
               "https://overseasadventuroustravel01.files.wordpress.com/2015/07/mountain-peaks.jpg?w=322&h=217",
@@ -22,18 +31,21 @@ const GameOverScreen = (props) => {
           style={styles.image}
           fadeDuration={300}
         /> */}
-      </View>
-      <View style={styles.resultContainer}>
-        <Text style={DefaultStyles.bodyText}>
-          Your phone needed{" "}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          get the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </Text>
-      </View>
+        </View>
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>
+            <Text style={DefaultStyles.bodyText}>
+              Your phone needed{" "}
+              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds
+              to get the number{" "}
+              <Text style={styles.highlight}>{props.userNumber}</Text>
+            </Text>
+          </Text>
+        </View>
 
-      <MainButton onPress={props.restartGame}>RESTART</MainButton>
-    </View>
+        <MainButton onPress={props.restartGame}>RESTART</MainButton>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -44,24 +56,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   imageContainer: {
-    width: 300,
-    height: 300,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
     borderColor: "black",
-    borderRadius: 150,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     overflow: "hidden",
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 20,
   },
   image: {
     width: "100%",
     height: "100%",
   },
   resultContainer: {
-    marginVertical: 20,
+    marginVertical: Dimensions.get("window").height / 40,
+    marginHorizontal: 30,
   },
   highlight: {
     color: Colors.primary,
     fontFamily: "open-sans-bold",
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
   },
 });
 
