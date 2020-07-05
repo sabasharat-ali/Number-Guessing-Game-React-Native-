77a8ecb25e1801db7e40119c42268398a0c4b299
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-05 16:12:24 +0500

    added safe area view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
@@ -62,10 +62,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Header title="GUESS A NUMBER" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
